685244df3057800d6f82c21455fde4f5fb6d3496
Michael Garnreiter <mgarnreiter@aol.com>
2019-11-12 19:51:49 +0100

    90% done

--- starter-code/bin/seeds.js
+++ starter-code/bin/seeds.js
@@ -33,19 +33,19 @@
 
 const movies = [
   {
-    name: "Toy Story 1",
+    title: "Toy Story 1",
     genre: "animation",
     plot:
       "Cupcake ipsum dolor sit amet apple pie cupcake danish. Fruitcake carrot cake sweet. Danish cake ice cream liquorice. Dragée chocolate cake jelly sweet brownie bear claw. Danish fruitcake soufflé. Jelly-o pudding donut chocolate bar cheesecake oat cake tootsie roll tart brownie. Jelly-o marzipan chocolate cake lollipop pastry. Bonbon topping lemon drops cake powder toffee soufflé tootsie roll jelly beans."
   },
   {
-    name: "Toy Story 2",
+    title: "Toy Story 2",
     genre: "animation",
     plot:
       "Pudding topping brownie muffin cupcake chocolate cake cake cookie bear claw. Chupa chups cake sesame snaps chocolate bar pie. Chocolate bar candy carrot cake gummies sugar plum chupa chups marzipan chupa chups bonbon."
   },
   {
-    name: "Toy Story 3",
+    title: "Toy Story 3",
     genre: "animation",
     plot:
       "Muffin croissant fruitcake sugar plum powder jelly-o gummies cookie. Cake cookie halvah carrot cake pastry bonbon marshmallow icing. Cheesecake biscuit sweet. Cake croissant muffin chocolate cake chocolate bar powder."
--- starter-code/routes/movies.js
+++ starter-code/routes/movies.js
@@ -2,6 +2,80 @@ const express = require("express");
 const router = express.Router();
 const Movies = require("../models/Movies");
 
-router.get("/", (req, res) => {
-  res.render("index.hbs");
+router.get("/", (req, res, next) => {
+  Movies.find({})
+    .then(movies => {
+      res.render("movies/index", { movies: movies });
+      //next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
+
+router.get("/new", (req, res) => {
+  res.render("movies/new.hbs");
+  //   next();
+});
+
+router.post("/", (req, res) => {
+  Movies.create({
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot
+  })
+    .then(movies => {
+      res.redirect("/movies");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/:id", (req, res) => {
+  Movies.findById(req.params.id)
+    .then(movies => {
+      res.render("movies/show", { movies });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/:id/edit", (req, res) => {
+  res.render("movies/edit.hbs");
+});
+
+router.get("/movies/:id/edit", (req, res, next) => {
+  // ???
+  Movies.findById(req.params.id)
+    .then(movies => {
+      movies.updateOne({
+        title: req.body.title,
+        genre: req.body.genre,
+        plot: req.body.plot
+      });
+      res.redirect("/movies");
+      next();
+      //   res.send("hi");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+router.get("/:id/delete", (req, res, next) => {
+  // ????
+  //res.send("here");
+  Movies.findByIdAndRemove(req.params.id)
+    .then(celebs => {
+      res.redirect("/movies");
+      next();
+      //   res.send("hi");
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
